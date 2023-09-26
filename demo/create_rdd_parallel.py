b8fe71538a1e7f8f1b45cfb67b670b38855aeb7d
shaikmalikbasha583 <shaikmalikbasha583@gmail.com>
2023-09-26 14:43:14 +0530

    create RDD code updated

--- demo/create_rdd_parallel.py
+++ demo/create_rdd_parallel.py
@@ -1,10 +1,21 @@
+import os
+import shutil
+
 from helper.spark_object import close_spark_session_object, get_spark_session_object
 
 spark = get_spark_session_object()
+par_dir = "sink/rdd/parallel"
+text_dir = "sink/rdd/text"
+
+if os.path.isdir(par_dir) or os.path.exists(par_dir):
+    shutil.rmtree(par_dir)
+if os.path.isdir(text_dir) or os.path.exists(text_dir):
+    shutil.rmtree(text_dir)
 
 ## parallelize()
 numRDD = spark.sparkContext.parallelize(range(10), numSlices=6)
 print(f"\nPartitions: {numRDD.getNumPartitions()}")
+numRDD.saveAsTextFile(par_dir)
 """
 Partitions: 6
 """
@@ -13,6 +24,7 @@ Partitions: 6
 ## textFile() method
 fileRDD = spark.sparkContext.textFile("data/names.txt", minPartitions=6)
 print(f"\nPartitions: {numRDD.getNumPartitions()}")
+fileRDD.saveAsTextFile(text_dir)
 """
 Partitions: 6
 """
